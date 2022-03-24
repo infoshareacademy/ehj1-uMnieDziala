b4b35977265d1ba45c63eb656f64da3147cc87fb
Mateusz Tarasewicz <Mateusz.Tarasewicz@ergohestia.pl>
2022-03-24 10:59:07 +0100

    Delete .gitignore in non-empty directory

--- src/main/java/com/isa/unasdziala/Main.java
+++ src/main/java/com/isa/unasdziala/Main.java
@@ -14,6 +14,5 @@ public static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         System.out.println("Run...");
         logger.info("\ntest loggera");
-        new NonWorkingDayService(new NonWorkingDaysRepository());
     }
 }
