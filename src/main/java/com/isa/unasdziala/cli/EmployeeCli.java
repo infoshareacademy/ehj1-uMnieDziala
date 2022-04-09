0a8244375c2f49bf30a1c964f58774c017e5783f
Pawel Majewski <pawel.majewski@ergohestia.pl>
2022-04-09 19:17:07 +0200

    employee Cli created

--- src/main/java/com/isa/unasdziala/cli/EmployeeCli.java
+++ src/main/java/com/isa/unasdziala/cli/EmployeeCli.java
@@ -1,12 +1,14 @@
 package com.isa.unasdziala.cli;
 
-import com.isa.unasdziala.domain.Day;
+import com.isa.unasdziala.domain.*;
 import com.isa.unasdziala.services.EmployeeService;
 import com.isa.unasdziala.services.NonWorkingDayService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class EmployeeCli {
@@ -78,6 +80,44 @@ public class EmployeeCli {
         //service.deleteEmployeeByLastName(getUserInput());
 
     }
+
+    public void addNewEmployee() {
+        logSTD.info("Enter employee name:");
+        String name = scanner.nextLine();
+        logSTD.info("Enter employee lastname:");
+        String lastname = scanner.nextLine();
+        logSTD.info("Enter employee phone number:");
+        String phone = scanner.nextLine();
+        logSTD.info("Enter employee email address:");
+        String email = scanner.nextLine();
+        logSTD.info("Enter employee city:");
+        String city = scanner.nextLine();
+        logSTD.info("Enter employee zip code:");
+        String code = scanner.nextLine();
+        logSTD.info("Enter employee post city:");
+        String postCity = scanner.nextLine();
+        logSTD.info("Enter employee street:");
+        String street = scanner.nextLine();
+        logSTD.info("Enter employee house number:");
+        String houseNumber = scanner.nextLine();
+        logSTD.info("Enter employee flat number:");
+        String flatnumber = scanner.nextLine();
+        logSTD.info("Enter employee department:");
+        String department = scanner.nextLine();
+        logSTD.info("Enter employee holidays:");
+        Float holidays = Float.valueOf(scanner.nextLine());
+
+
+        Address address= new Address(city, code, postCity, street, houseNumber,flatnumber);
+        Contact contact= new Contact(phone, email);
+
+        testEmployees.add(new Employee(name, lastname, contact, address, Department.DEPARTAMENT_FINANSOW, 22.5f));
+
+
+            return input;
+        }
+    }
+
 
 }
 
