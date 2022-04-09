d07158f4e383a4c13872a99bd892fde0acd81f99
Mateusz Tarasewicz <Mateusz.Tarasewicz@ergohestia.pl>
2022-04-09 14:31:10 +0200

    employee service

--- src/main/java/com/isa/unasdziala/services/EmployeeService.java
+++ src/main/java/com/isa/unasdziala/services/EmployeeService.java
@@ -4,6 +4,7 @@ import com.isa.unasdziala.dto.EmployeeDto;
 import com.isa.unasdziala.repository.EmployeesRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class EmployeeService {
     private EmployeesRepository employeesRepository;
@@ -31,7 +32,21 @@ public class EmployeeService {
                 );
     }
 
+    public EmployeeDto deleteEmployee(String firstName, String lastName) throws IllegalArgumentException {
+        return employeesRepository.delete(firstName, lastName)
+                .orElseThrow(
+                        () -> new IllegalArgumentException(
+                                "Employee with this first name and last name does not exist: " + firstName + " " + lastName
+                        )
+                );
+    }
 
-
-
+    public EmployeeDto updateEmployee(EmployeeDto employeeDto) throws IllegalArgumentException {
+        Optional<EmployeeDto> updatedEmployeeDto = employeesRepository.update(employeeDto);
+        return updatedEmployeeDto.orElseThrow(
+                () -> new IllegalArgumentException(
+                        "Employee with this first name and last name does not exist: " + employeeDto.getFirstName() + " " + employeeDto.getLastName()
+                )
+        );
+    }
 }
