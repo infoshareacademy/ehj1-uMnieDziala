50aa6c2e95ac7057eb2211f1021d37ed738efd4e
Mateusz Tarasewicz <Mateusz.Tarasewicz@ergohestia.pl>
2022-04-27 12:30:56 +0200

    Springboot empty project

---
+++ src/main/java/com/isa/unasdziala/Ehj1UNasDzialaApplication.java
@@ -0,0 +1,13 @@
+package com.isa.unasdziala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ehj1UNasDzialaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ehj1UNasDzialaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isa/unasdziala/Ehj1UNasDzialaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.unasdziala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ehj1UNasDzialaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
