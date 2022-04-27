91286eac54a3e8ffa9e384db2cb1bc21bcadcde7
daniel-surowiec <dabiel.surowiec@ergohestia.pl>
2022-04-27 13:21:12 +0200

    update HolidayDto

--- src/main/java/com/isa/unasdziala/dto/HolidayDto.java
+++ src/main/java/com/isa/unasdziala/dto/HolidayDto.java
@@ -1,10 +1,12 @@
 package com.isa.unasdziala.dto;
 
+import com.isa.unasdziala.model.Employee;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -12,9 +14,11 @@ import java.time.LocalDate;
 public class HolidayDto {
     private Long id;
     private LocalDate date;
+    private Set<Employee> employees;
 
-    public HolidayDto(Long id, LocalDate date) {
+    public HolidayDto(Long id, LocalDate date, Set<Employee> employees) {
         this.id = id;
         this.date = date;
+        this.employees = employees;
     }
 }
