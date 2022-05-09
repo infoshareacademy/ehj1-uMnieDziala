fb578b467f5af399c830ed58cca5aef567b85bd2
daniel-surowiec <dabiel.surowiec@ergohestia.pl>
2022-05-09 15:38:13 +0200

    merge

--- src/test/java/com/isa/unasdziala/controller/HolidayControllerTest.java
+++ src/test/java/com/isa/unasdziala/controller/HolidayControllerTest.java
@@ -1,24 +1,28 @@
 package com.isa.unasdziala.controller;
 
 
-import com.isa.unasdziala.request.DeleteHolidaysRequest;
+import com.isa.unasdziala.dto.HolidayDto;
+import com.isa.unasdziala.exception.ResourceNotFoundException;
+import com.isa.unasdziala.model.Employee;
+import com.isa.unasdziala.request.AddHolidaysRequest;
 import com.isa.unasdziala.service.HolidayService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
