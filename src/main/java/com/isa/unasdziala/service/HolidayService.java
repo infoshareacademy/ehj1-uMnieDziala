e1392770fcf67e703d012a8fd40262f055bef665
daniel-surowiec <dabiel.surowiec@ergohestia.pl>
2022-05-09 14:06:11 +0200

    Holiday service AddHoliday + deleteById

--- src/main/java/com/isa/unasdziala/service/HolidayService.java
+++ src/main/java/com/isa/unasdziala/service/HolidayService.java
@@ -5,6 +5,7 @@ import com.isa.unasdziala.dto.HolidayDto;
 import com.isa.unasdziala.exception.ResourceNotFoundException;
 import com.isa.unasdziala.model.Employee;
 import com.isa.unasdziala.model.Holiday;
+import com.isa.unasdziala.repository.EmployeeRepository;
 import com.isa.unasdziala.repository.HolidayRepository;
 import com.isa.unasdziala.request.AddHolidaysRequest;
 import com.isa.unasdziala.request.DeleteHolidaysRequest;
@@ -15,6 +16,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,13 +26,13 @@ import static org.aspectj.runtime.internal.Conversions.longValue;
 @Service
 public class HolidayService {
     private final HolidayRepository holidayRepository;
-    private final EmployeeService employeeService;
+    private final EmployeeRepository employeeRepository;
     private final ModelMapper modelMapper;
     private final HolidayConfiguration holidayConfiguration;
 
-    public HolidayService(HolidayRepository holidayRepository, EmployeeService employeeService, ModelMapper modelMapper, HolidayConfiguration holidayConfiguration) {
+    public HolidayService(HolidayRepository holidayRepository, EmployeeService employeeService, ModelMapper modelMapper, EmployeeRepository employeeRepository, HolidayConfiguration holidayConfiguration) {
         this.holidayRepository = holidayRepository;
-        this.employeeService = employeeService;
+        this.employeeRepository = employeeRepository;
         this.modelMapper = modelMapper;
         this.holidayConfiguration = holidayConfiguration;
     }
@@ -49,7 +51,8 @@ public class HolidayService {
     }
 
     public List<HolidayDto> addHoliday(Long userId, AddHolidaysRequest addHolidaysRequest) {
-        Employee employee = employeeService.findEmployeeById(userId);
+
+        Optional<Employee> employee = employeeRepository.findById(userId);
 
         Set<LocalDate> bussyDaysFromEmployeeHolidaysDays = holidayRepository.findAllByEmployeesId(userId).stream()
                 .map(Holiday::getDate)
@@ -67,16 +70,33 @@ public class HolidayService {
         addHolidaysRequest.getDates().stream()
                 .filter(date -> !bussyDays.contains(date))
                 .filter(date -> date.getDayOfWeek() != DayOfWeek.SATURDAY || date.getDayOfWeek() != DayOfWeek.SUNDAY)
-                .limit(longValue(employee.getHolidays()))
+                .limit(longValue(employee.get().getHolidays()))
                 .collect(Collectors.toSet());
 
-        employee.setHolidays(employee.getHolidays() - addHolidaysRequest.getDates().size());
-        employee.setHolidayDays(employee.getHolidayDays().add());
+        Set<Holiday> holidaysToAdd = addHolidaysRequest.getDates().stream()
+                .map(date -> new Holiday(date))
+                .collect(Collectors.toSet());
 
-        return null;
+        employee.get().setHolidays(employee.get().getHolidays() - addHolidaysRequest.getDates().size());
+        employee.get().getHolidayDays().addAll(holidaysToAdd);
+        employeeRepository.save(employee.get());
+
+        return holidaysToAdd.stream()
+                .map(holiday -> modelMapper.map(holiday, HolidayDto.class))
+                .toList();
     }
 
     public void deleteById(Long userId, DeleteHolidaysRequest deleteHolidaysRequest) {
-        throw new RuntimeException("Not implemented");
+        Optional<Employee> employee = employeeRepository.findById(userId);
+
+        Set<Holiday> holidaysToDelete = deleteHolidaysRequest.getHolidaysId().stream()
+                .map(id -> findHolidayById(userId, id))
+                .map(holiday -> modelMapper.map(holiday, Holiday.class))
+                .collect(Collectors.toSet());
+
+        employee.get().setHolidays(employee.get().getHolidays() + deleteHolidaysRequest.getHolidaysId().size());
+        employee.get().getHolidayDays().removeAll(holidaysToDelete);
+        employeeRepository.save(employee.get());
+
     }
 }
